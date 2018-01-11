ecf3315cb93c075ca8f2cb454bf07a3c9030c455
Prokti <thibaud.berdin@maison-berdin.com>
2018-01-11 09:40:43 +0100

    Oh my god !

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Contact;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-
+use App\Entity\Contact;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 class ContactController extends Controller
 {
     /**
@@ -14,12 +19,12 @@ class ContactController extends Controller
      */
     public function index()
     {
-        // replace this line with your own code!
+
+
         $em = $this->getDoctrine()->getRepository(Contact::class)->findAll();
 
-        return $this->render('contact/list.html.twig', [
-            'em' => $em
-        ]);
+        return $this->json($em);
+
 
 
     }
